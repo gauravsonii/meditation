12405e55cf4e3868e4dc23089aa6441b8b84970e
GAURAV <gaauuuraavvv@gmail.com>
2025-07-29 15:33:15 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,74 @@
+const app = () => {
+    const song = document.querySelector('.song');
+    const play = document.querySelector('.play');
+    const outline = document.querySelector('.moving-outline circle');
+    const video = document.querySelector('.vid-container video');
+    //Sounds
+    const sounds = document.querySelectorAll('.sound-picker button');
+    //Time Display
+    const timeDisplay = document.querySelector('.time-display');
+    const timeSelect = document.querySelectorAll('.time-select button');
+    //Get the length of the circle
+    const outlineLength = outline.getTotalLength();
+    
+    let fakeDuration = 600;
+
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDashoffset = outlineLength;
+
+
+    //play different sounds
+    sounds.forEach(sound => {
+        sound.addEventListener('click', function () {
+            song.src = this.getAttribute('data-sound');
+            video.src = this.getAttribute('data-video');
+            checkPlaying(song);
+       }) 
+    });
+    // play sound
+    play.addEventListener('click', () => {
+        checkPlaying(song);
+    });
+    timeSelect.forEach(option => {
+        option.addEventListener('click', function () {
+            fakeDuration = this.getAttribute('data-time');
+            timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}`;
+        })
+    })
+
+    // function for cheacking if song is playing or not
+    const checkPlaying = song => {
+        if (song.paused) {
+            song.play();
+            video.play();
+            play.src = "./svg/pause.svg";
+        } else {
+            song.pause();
+            video.pause();
+            play.src = "./svg/play.svg";
+        }
+    };
+
+    // animate the circle
+    song.ontimeupdate = () => {
+        let currentTime = song.currentTime;
+        let elapsed = fakeDuration - currentTime;
+        let seconds = Math.floor(elapsed % 60);
+        let minutes = Math.floor(elapsed / 60);
+        
+        //animate the circle
+        let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+        outline.style.strokeDashoffset = progress;
+        // display time
+        timeDisplay.textContent = `${minutes}:${seconds}`;
+
+        if (currentTime >= fakeDuration) {
+            song.pause();
+            video.pause();
+            song.currentTime = 0;
+            play.src = "./svg/play.svg";
+        }
+    }
+};
+
+app();
